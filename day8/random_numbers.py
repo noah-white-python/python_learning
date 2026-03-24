1547d6f78aaca56ca1dbe0a29b16b6bd4fbc10e7
noah_white <2480530563@qq.com>
2026-03-24 15:06:23 +0800

    array and numpy

--- day8/random_numbers.py
+++ day8/random_numbers.py
@@ -10,7 +10,7 @@ print(c)
 x = [1,2,3,4,5]
 print(np.random.choice(x))
 print(np.random.choice(x,3))
-print(np.random.choice(x,replace = False))
+print(np.random.choice(x,3,replace = False))
 
 np.random.shuffle(x)
 print(x)
